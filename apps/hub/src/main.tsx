15132d189bd356ad2d712cddfcfb2359f5b74dd5
dillybert <dillybert@github.com>
2026-03-10 15:11:04 +0500

    Initial commit

--- apps/hub/src/main.tsx
+++ apps/hub/src/main.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-
 import "@workspace/ui/globals.css"
-import { App } from "./App.tsx"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { App } from "./App"
+import { ThemeProvider } from "@workspace/ui/providers/theme-provider"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="mycity-frontend-theme">
       <App />
     </ThemeProvider>
   </StrictMode>
